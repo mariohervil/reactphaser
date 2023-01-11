a54b227f76c9893d6ab4f480d8edab324e128885
mariohervil <mariohervil@campus.monlau.com>
2023-01-11 18:00:55 +0100

    React - Phaser funciona

---
+++ src/Game.js
@@ -0,0 +1,106 @@
+/* eslint-disable no-unused-vars */
+import Phaser from 'phaser'
+import fondo from './fondo.jpg'
+import lineas from './lineas.jpg'
+import pelota from './pelota.png'
+export class Game extends Phaser.Scene {
+  constructor () {
+    super({ key: 'game' })
+  }
+
+  preload () {
+    this.load.image('fondo', fondo)
+    // this.load.image('lineas', lineas)
+    // this.load.image('pelota', pelota)
+  }
+
+  create () {
+    this.physics.world.setBoundsCollision(false, false, true, true)
+    this.add.image(750, 400, 'fondo')
+
+    this.canoa1 = this.physics.add.image(100, 400, 'lineas').setImmovable()
+    this.canoa1.body.allowGravity = false
+    this.canoa1.setCollideWorldBounds(true)
+
+    this.p1 = this.input.keyboard.createCursorKeys()
+    // ------------------------------------------------------------------------//
+    this.canoa2 = this.physics.add.image(1400, 400, 'lineas').setImmovable()
+    this.canoa2.body.allowGravity = false
+    this.canoa2.setCollideWorldBounds(true)
+
+    this.p2 = this.input.keyboard.addKeys('A,W,S,D')
+    // ------------------------------------------------------------------------//
+    this.pelota = this.physics.add.image(750, 100, 'pelota')
+    this.pelota.setCollideWorldBounds(true)
+
+    this.pelota.setBounce(1.1)
+
+    let velocity = 100 * Phaser.Math.Between(1.3, 2)
+    if (Phaser.Math.Between(0, 10) > 5) {
+      velocity = 0 - velocity
+    }
+
+    this.pelota.setVelocity(velocity, 10)
+    // ------------------------------------------------------------------------//
+
+    this.physics.add.collider(this.pelota, this.canoa1)
+    this.physics.add.collider(this.pelota, this.canoa2)
+
+    this.score1 = 0
+    this.score2 = 0
+
+    this.scoreText1 = this.add.text(620, 16, this.score1, { fontSize: '100px', fill: '#FFFFFF' })
+
+    this.scoreText2 = this.add.text(820, 16, this.score2, { fontSize: '100px', fill: '#FFFFFF' })
+  }
+
+  update () {
+    if (this.p1.up.isDown) {
+      this.canoa2.setVelocityY(-250)
+    } else if (this.p1.down.isDown) {
+      this.canoa2.setVelocityY(250)
+    } else {
+      this.canoa2.setVelocityY(0)
+    }
+    // ----------------------------------------//
+    if (this.p2.W.isDown) {
+      this.canoa1.setVelocityY(-250)
+    } else if (this.p2.S.isDown) {
+      this.canoa1.setVelocityY(250)
+    } else {
+      this.canoa1.setVelocityY(0)
+    }
+
+    if (this.pelota.x < 0) {
+      this.score2 += 1
+      this.scoreText2.setText(this.score2)
+      this.pelota.setPosition(750, 100)
+
+      let velocity = 100 * Phaser.Math.Between(1.3, 2)
+      if (Phaser.Math.Between(0, 10) > 5) {
+        velocity = 0 - velocity
+      }
+
+      this.pelota.setVelocity(velocity, 10)
+    } else if (this.pelota.x > 1500) {
+      this.score1 += 1
+      this.scoreText1.setText(this.score1)
+      this.pelota.setPosition(750, 100)
+
+      let velocity = 100 * Phaser.Math.Between(1.3, 2)
+      if (Phaser.Math.Between(0, 10) > 5) {
+        velocity = 0 - velocity
+      }
+
+      this.pelota.setVelocity(velocity, 10)
+    }
+    if (this.score1 === 6) {
+      this.scene.pause()
+      this.p1win = this.add.text(350, 350, 'Player 1 wins', { fontSize: '100px', fill: '#FFFFFF' })
+    }
+    if (this.score2 === 6) {
+      this.scene.pause()
+      this.p2win = this.add.text(350, 350, 'Player 2 wins', { fontSize: '100px', fill: '#FFFFFF' })
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Phaser from 'phaser'
+// import logoImg from './assets/logo.png'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.js'
+import { Game } from './Game.js'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const config = {
+  type: Phaser.AUTO,
+  width: 1500,
+  height: 800,
+  scene: [Game],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 100 },
+      debug: false
+    }
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+const game = new Phaser.Game(config)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+ <App></App>
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
